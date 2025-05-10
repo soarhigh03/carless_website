baf15bded0b8a7b117abdcf5927341383235f47c
Eunwoo Choi <soarhigh03@gmail.com>
2025-05-10 21:56:44 +0900

    푸터 configure + 개인정보처리방침 페이지 로드

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AdminLogin from './components/AdminLogin';
+import Privacy from './pages/Privacy';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/privacy" element={<Privacy />} />
       </Routes>
     </BrowserRouter>
   );
