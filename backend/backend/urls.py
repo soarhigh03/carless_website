7a64bd84195651342072f85cb74cf30ccbc97bfe
Eunwoo Choi <soarhigh03@gmail.com>
2025-08-08 11:18:36 +0900

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from core.views import healthcheck   # ✅ 추가
 
 urlpatterns = [
+    path('healthcheck/', healthcheck),    # ✅ 루트 핑 추가
     path('api/', include('core.urls')),
-    path('admin/', admin.site.urls), 
-    re_path(r"^(?!admin|api|static).*", TemplateView.as_view(template_name="index.html")),      # Django 관리자 페이지 
+    path('admin/', admin.site.urls),
+    re_path(r"^(?!admin|api|static).*", TemplateView.as_view(template_name="index.html")),
 ]
