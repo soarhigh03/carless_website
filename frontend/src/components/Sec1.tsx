f894f284825e4f30ff2a82ebe9daaa4a2ab5272b
doonghwi <ehdgnlans@gmail.com>
2025-08-08 22:08:07 +0900

    sec1 이미지 3장 업로드

--- frontend/src/components/Sec1.tsx
+++ frontend/src/components/Sec1.tsx
@@ -11,16 +11,24 @@ export default function Sec1() {
   return (
     <section className="mt-4 w-full bg-gray-50">
       <img
-        src="/images/hero-truck.png"
-        alt="Section 1 banner"
+        src="/images/sec1_pic1.png"
+        alt="Section 1 banner1"
         className="w-full h-auto object-contain block cursor-pointer"
         style={{ maxWidth: '100vw' }}
         onClick={handleClick}
       />
-      
+
       <img
-        src="/images/hero-truck.png"
-        alt="Section 1 banner"
+        src="/images/sec1_pic2.png"
+        alt="Section 1 banner2"
+        className="w-full h-auto object-contain block cursor-pointer"
+        style={{ maxWidth: '100vw' }}
+        onClick={handleClick}
+      />
+
+      <img
+        src="/images/sec1_pic3.png"
+        alt="Section 1 banner3"
         className="w-full h-auto object-contain block cursor-pointer"
         style={{ maxWidth: '100vw' }}
         onClick={handleClick}
